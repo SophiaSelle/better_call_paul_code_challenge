9cf7d83291701e4e1c44b6b0db519668d2660297
Sophia Selle <57111057+SophiaSelle@users.noreply.github.com>
2023-05-09 16:34:40 +0200

    add calculate division function, some refactoring

--- src/main/java/de/bcxp/challenge/data_processing.java
+++ src/main/java/de/bcxp/challenge/data_processing.java
@@ -2,6 +2,8 @@ package de.bcxp.challenge;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -13,19 +15,23 @@ public class data_processing {
     String[][] dataTable;
     String[] columnNames;
 
-    public data_processing(String data_filename){
+    public data_processing(String data_filename, char separator){
         try {
-            load_data(data_filename);
+            load_data(data_filename, separator);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
     
-    public void load_data(String filename) throws IOException {
+    public void load_data(String filename, char separator) throws IOException {
         FileReader filereader = new FileReader(filename);
+        CSVParser parser = new CSVParserBuilder()
+                .withSeparator(separator)
+                .build();
         CSVReader csvReader = new CSVReaderBuilder(filereader)
-                                  .build();
+                .withCSVParser(parser)
+                .build();
         this.columnNames = csvReader.readNext();
         List<String[]> data_list = csvReader.readAll();                        
         this.dataTable = data_list.toArray(new String[data_list.size()][]);
@@ -53,6 +59,30 @@ public class data_processing {
             differences[i]= a_value - b_value;
         }
         return differences;
+    }
+
+    public Float[] calculate_division(String column_name_a, String column_name_b){
+        int column_a_index = Arrays.asList(this.columnNames).indexOf(column_name_a);
+        int column_b_index = Arrays.asList(this.columnNames).indexOf(column_name_b);
+        
+        int column_length = this.dataTable.length;
+        Float[] results = new Float[column_length];
+
+        for(int i=0; i<column_length; i++){
+            //Get the values in row i
+            NumberFormat format = NumberFormat.getInstance();
+            Float a_value = (float) 0;
+            Float b_value = (float) 0;
+            try {
+                a_value = format.parse(this.dataTable[i][column_a_index]).floatValue();
+                b_value = format.parse(this.dataTable[i][column_b_index]).floatValue();
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            results[i]= a_value/b_value;
+        }
+        return results;
     }
 
     public String getNameOfMax(int name_column_index, Float[] value_column){
