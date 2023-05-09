eaf1f84831de4b4822b9de8362ca32b7fe28c95d
Sophia Selle <57111057+SophiaSelle@users.noreply.github.com>
2023-05-09 16:45:37 +0200

    add solution

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -1,5 +1,7 @@
 package de.bcxp.challenge;
 
+import java.util.Arrays;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -11,13 +13,14 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
+        data_processing temperature = new data_processing("programming-challenge/src/main/resources/de/bcxp/challenge/weather.csv", ',');
+        data_processing population = new data_processing("programming-challenge/src/main/resources/de/bcxp/challenge/countries.csv", ';');
 
-        // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        String dayWithSmallestTempSpread = temperature.getNameOfMaxDifference("MxT", "MnT", "Day");
         System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
 
-        String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
+        //TODO: Better special character handling
+        String countryWithHighestPopulationDensity = population.getNameOfMaxDivision("Population", "Area (kmÂ²)", "Name");
         System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
     }
 }
